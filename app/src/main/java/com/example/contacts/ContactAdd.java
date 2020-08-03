bfab167521c48c8814bdf1143ed33be0399b5d50
Ashish4n8 <ashishnagaraj123@gmail.com>
2020-08-03 21:31:21 +0900

    August 3

--- app/src/main/java/com/example/contacts/ContactAdd.java
+++ app/src/main/java/com/example/contacts/ContactAdd.java
@@ -3,12 +3,47 @@ package com.example.contacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ContactAdd extends AppCompatActivity {
+    Spinner cat;
+    ImageView img;
+    EditText fName,lName,num1,num2,email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_add);
+        cat = findViewById(R.id.dropdown);
+        img = findViewById(R.id.img);
+        fName = findViewById(R.id.fName);
+        lName = findViewById(R.id.lName);
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        email = findViewById(R.id.email);
+
+        String options[] = getResources().getStringArray(R.array.options);
+        ArrayAdapter ada = new ArrayAdapter(this, android.R.layout.simple_spinner_item,options);
+        ada.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cat.setAdapter(ada);
+
+        cat.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String option = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(ContactAdd.this, option, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
